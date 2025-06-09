4b89b7e9bfc62659caa0c3ce82864fd485d314ae
symonrodriguesl <154328613+symonrodriguesl@users.noreply.github.com>
2025-06-09 21:43:07 +0200

    lot size calculator commit

--- app/page.tsx
+++ app/page.tsx
@@ -7,6 +7,7 @@ import { ConnectSupabaseSteps } from "@/components/tutorial/connect-supabase-ste
 import { SignUpUserSteps } from "@/components/tutorial/sign-up-user-steps";
 import { hasEnvVars } from "@/lib/utils";
 import Link from "next/link";
+import { LotSizeCalculator } from "@/components/lot-size-calculator";
 
 export default function Home() {
   return (
@@ -25,6 +26,7 @@ export default function Home() {
         </nav>
         <div className="flex-1 flex flex-col gap-20 max-w-5xl p-5">
           <Hero />
+          <LotSizeCalculator/>
           <main className="flex-1 flex flex-col gap-6 px-4">
             <h2 className="font-medium text-xl mb-4">Next steps</h2>
             {hasEnvVars ? <SignUpUserSteps /> : <ConnectSupabaseSteps />}
