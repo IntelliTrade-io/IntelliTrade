2f3d6b08e05f191d206ebec4fcf20844183fdd4f
symonrodriguesl <154328613+symonrodriguesl@users.noreply.github.com>
2025-09-25 22:06:30 +0200

    newsletter

---
+++ scripts/newsletter.js
@@ -0,0 +1,23 @@
+
+  window.REQUIRED_CODE_ERROR_MESSAGE = 'Please choose a country code';
+  window.LOCALE = 'en';
+  window.EMAIL_INVALID_MESSAGE = window.SMS_INVALID_MESSAGE = "The information provided is invalid. Please review the field format and try again.";
+
+  window.REQUIRED_ERROR_MESSAGE = "This field cannot be left blank. ";
+
+  window.GENERIC_INVALID_MESSAGE = "The information provided is invalid. Please review the field format and try again.";
+
+
+
+
+  window.translation = {
+    common: {
+      selectedList: '{quantity} list selected',
+      selectedLists: '{quantity} lists selected',
+      selectedOption: '{quantity} selected',
+      selectedOptions: '{quantity} selected',
+    }
+  };
+
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  var AUTOHIDE = Boolean(0);
